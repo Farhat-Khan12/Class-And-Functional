3ef182d94fbab5d4e859f228b77d11b2eb2b3613
Farhat-Khan12 <90671950+Farhat-Khan12@users.noreply.github.com>
2022-03-07 19:26:13 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import FunctionalComponent from './component/Functional';
+import ClassComponent from './component/Class';
+
+function App() {
+  return (
+    <div className="App">
+      <FunctionalComponent/>
+      <ClassComponent/>
+    </div>
+  );
+}
+
+export default App;
