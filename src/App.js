493cc7a4ccd65263262534957453746263df257c
Farhat-Khan12 <90671950+Farhat-Khan12@users.noreply.github.com>
2022-03-11 19:40:02 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import './App.css';
 import FunctionalComponent from './component/Functional';
 import ClassComponent from './component/Class';
-
+import ImageSlider from './ImageSlider';
 function App() {
   return (
     <div className="App">
       <FunctionalComponent/>
       <ClassComponent/>
+      <ImageSlider/>
     </div>
   );
 }
